5735969333519135b10252855cbdf8328c601ae4
vinicius-firmino35 <vinicius.candido.firmino@ccc.ufcg.edu.br>
2025-02-12 06:11:05 -0300

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -78,4 +78,29 @@ public static int[] mergeOrdenadosDistintos(int[] a, int[] b) {
         while (j < n2) {
             v[k++] = direita[j++];
         }
+
+	 public static void merge(int[] v, int left, int middle, int right) {
+		 int[] helper = new int[v.length];
+		 
+	        for (int i = left; i <= right; i++) {
+	            helper[i] = v[i];
+	        }
+	        
+	        int i = left;
+	        int j = middle + 1;
+	        int k = left;
+	        
+	        while (i <= middle && j <= right) {       
+	            if (helper[i] <= helper[j]) {
+	                v[k++] = helper[i++];
+	     
+	            } else {
+	                v[k++] = helper[j++];
+	            }
+	        }
+	
+	        while (i <= middle) {
+	            v[k++] = helper[i++];
+	        }
+	 }
     }
